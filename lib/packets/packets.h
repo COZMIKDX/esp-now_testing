4019f9e33d8cafe8bdf1029f1432601c1b527595
COZMIKDX <tacocatdx@gmail.com>
2026-08-16 03:29:25 -0500

    create header file for holding shared packet structures.

---
+++ lib/packets/packets.h
@@ -0,0 +1,10 @@
+#ifndef MY_PACKETS
+#define MY_PACKETS
+#endif
+
+// Be sure to use data types that specify the number of bits, such as uint8_t.
+// For floating point values, do not use long, use float.
+
+typedef struct struct_message {
+    char message[32];
+} struct_message;
