d0a6125f145e3aed929a51ce8216d4e93f109b78
COZMIKDX <tacocatdx@gmail.com>
2026-08-16 19:25:12 -0500

    changed example struct to be packed.

--- lib/packets/packets.h
+++ lib/packets/packets.h
@@ -4,7 +4,8 @@
 
 // Be sure to use data types that specify the number of bits, such as uint8_t.
 // For floating point values, do not use long, use float.
-
-typedef struct struct_message {
+// Making the struct packed prevents the compiler from adding any sort of padding between variables.
+// Otherwise, you would need to order the variables from biggest size to smallest in the struct.
+typedef struct __attribute__((packed)) struct_message {
     char message[32];
 } struct_message;
